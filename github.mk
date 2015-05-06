5f27fc98aef10e16c8c8e8c33775e611e157891c
Craig Barnes <Cr@igBarn.es>
2015-05-06 07:45:10 +0100

    Write JSON responses to marker files

--- github.mk
+++ github.mk
@@ -2,14 +2,12 @@ MAIN_PREDEFINED = 1.0 1.1 2.1 2.2 2.4 2.5 3.0 3.1 3.2 4.0 5.0 5.1 5.2.0 5.3.0
 MAIN_GENERATED  = $(filter-out $(MAIN_PREDEFINED), $(MAIN_VERSIONS))
 RELEASES_MAIN   = $(addprefix releases/, $(addsuffix .json, $(MAIN_GENERATED)))
 RELEASES_WORK   = $(addprefix releases/, $(addsuffix .json, $(WORK_VERSIONS)))
+GITHUB_API      = https://api.github.com
 
 releases: $(addprefix .markers/gh-, $(MAIN_VERSIONS) $(WORK_VERSIONS))
 
 .markers/gh-%: releases/%.json | tarballs/lua-%.tar.gz .curl .markers/
-	curl -K .curl --data @$< https://api.github.com/repos/lua/lua/releases
-	# TODO: Parse the "upload_url" field from the JSON response and use
-	#       it to upload the original release tarball as an "asset"
-	touch $@
+	curl -K .curl --data @$< -o $@ $(GITHUB_API)/repos/lua/lua/releases
 
 $(RELEASES_MAIN): releases/%.json: template.json.in
 	sed 's/$$TAGNAME/$*/g; s/$$PRERELEASE/false/g' $< > $@
@@ -28,4 +26,3 @@ json: $(RELEASES_MAIN) $(RELEASES_WORK)
 
 
 .PHONY: releases json
-
